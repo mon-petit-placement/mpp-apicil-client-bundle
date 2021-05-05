aebec5e7b4dea99cef7c85551c8493abf4f6a271
celine.c <celine@monpetitplacement.fr>
2021-05-05 10:18:40 +0200

    fix/acc-client_fix-class-name-and-add-missing-class_cc

---
+++ Model/ResultatDeCreationResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Mpp\ApicilClientBundle\Model;
+
+class ResultatDeCreationResource
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
