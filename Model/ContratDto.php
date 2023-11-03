bd67040510f9f7bdc472d79d047006dc1d4c1edc
Dorian C <d.chereault@gmail.com>
2023-11-03 17:49:27 +0100

    fix: add public

--- Model/ContratDto.php
+++ Model/ContratDto.php
@@ -6,13 +6,13 @@ use DateTime;
 
 class ContratDto
 {
-    const  LIBELLE_SITUATION_RENONCE = 'RENONCE';
+    public const  LIBELLE_SITUATION_RENONCE = 'RENONCE';
 
-    const LIBELLE_SITUATION_RACHAT_TOTAL = 'RACHAT_TOTAL';
+    public const LIBELLE_SITUATION_RACHAT_TOTAL = 'RACHAT_TOTAL';
 
-    const LIBELLE_SITUATION_SANS_ANNULE = 'ANNULE';
+    public const LIBELLE_SITUATION_SANS_ANNULE = 'ANNULE';
 
-    const LIBELLE_SITUATION_ACTIF = 'ACTIF';
+    public const LIBELLE_SITUATION_ACTIF = 'ACTIF';
 
     /**
      * @var bool|null
